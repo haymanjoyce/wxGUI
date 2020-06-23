7e811e4b31fc12590c59b6f4606ace8fa07070f2
haymanjoyce <haymanjoyce@gmail.com>
2020-06-23 11:12:43 +0100

    Developing TreeCtrl.

--- main.py
+++ main.py
@@ -15,15 +15,28 @@ class TreePanel(wx.Panel):
         wx.Panel.__init__(self, parent)
 
         self.tree = TreeCtrl(self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TR_HAS_BUTTONS | wx.TR_EDIT_LABELS)
+        self.Bind(wx.EVT_SIZE, self.OnSize)
 
-        self.root = self.tree.AddRoot('Something goes here')
-        self.tree.SetPyData(self.root, ('key', 'value'))
-        os = self.tree.AppendItem(self.root, 'Operating Systems')
-        self.tree.Expand(self.root)
+        root = self.tree.AddRoot("Level 0 Item")
+        self.tree.SetItemData(root, None)
+
+        for l1_row in range(3):
+            l1_item = self.tree.AppendItem(parent=root, text=f'Level 1 Item: {l1_row}')
+            self.tree.SetItemData(l1_item, None)
+
+            for l2_row in range(3):
+                l2_item = self.tree.AppendItem(l1_item, f'Level 2 Item: {l2_row}')
+                self.tree.SetItemData(l2_item, None)
+
+        self.tree.ExpandAll()
 
         sizer = wx.BoxSizer(wx.VERTICAL)
         sizer.Add(self.tree, 0, wx.EXPAND)
         self.SetSizer(sizer)
+
+    def OnSize(self, event):
+        w, h = self.GetClientSize()
+        self.tree.SetSize(0, 0, w, h)
 
 
 class Frame(wx.Frame):

54169317d60faa5f2ec2aa79d04d09d47bff70f9
haymanjoyce <haymanjoyce@gmail.com>
2020-07-01 11:01:53 +0100

    Establishing which tree class to use.

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
 #!/usr/bin/env python
 
+# todo see if you can get images to work with gizmos
+# todo remove check boxes
+# todo add item types
+# todo export data
+# todo import data
+# todo item data panel
+
 import wx
+import wx.dataview
+import wx.lib.gizmos as gizmos
 import images
 
 
 class TreeCtrl(wx.TreeCtrl):
     def __init__(self, parent, id, pos, size, style):
         wx.TreeCtrl.__init__(self, parent, id, pos, size, style)
+
+        # Does not have columns
+
+
+class TreeListCtrlA(wx.dataview.TreeListCtrl):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Does have SetImageList()
+
+
+class TreeListCtrlB(gizmos.TreeListCtrl):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Does not have SetImageList()
+        # Has more user-friendly API
 
 
 class ImageList(wx.ImageList):
@@ -16,9 +42,8 @@ class ImageList(wx.ImageList):
         self.folder = self.Add(wx.ArtProvider.GetBitmap(wx.ART_FOLDER, wx.ART_OTHER, image_size))
         self.folder_open = self.Add(wx.ArtProvider.GetBitmap(wx.ART_FOLDER_OPEN, wx.ART_OTHER, image_size))
         self.file = self.Add(wx.ArtProvider.GetBitmap(wx.ART_NORMAL_FILE, wx.ART_OTHER, image_size))
-        self.fil_open = self.Add(wx.ArtProvider.GetBitmap(wx.ART_FILE_OPEN, wx.ART_OTHER, image_size))
-        self.custom_1 = self.Add(images.Smiles.GetBitmap())
-        print(self.GetImageCount())
+        self.file_open = self.Add(wx.ArtProvider.GetBitmap(wx.ART_FILE_OPEN, wx.ART_OTHER, image_size))
+        self.smiles = self.Add(images.Smiles.GetBitmap())
 
 
 class TreePanel(wx.Panel):
@@ -34,8 +59,8 @@ class TreePanel(wx.Panel):
 
         root = self.tree.AddRoot("Level 0 Item")
         self.tree.SetItemData(root, None)
-        self.tree.SetItemImage(root, self.il.folder, wx.TreeItemIcon_Normal)
-        self.tree.SetItemImage(root, self.il.folder_open, wx.TreeItemIcon_Expanded)
+        self.tree.SetItemImage(root, self.il.smiles, wx.TreeItemIcon_Normal)
+        self.tree.SetItemImage(root, self.il.smiles, wx.TreeItemIcon_Expanded)
 
         for l1_row in range(3):
             l1_item = self.tree.AppendItem(parent=root, text=f'Level 1 Item: {l1_row}')
@@ -47,9 +72,70 @@ class TreePanel(wx.Panel):
                 l2_item = self.tree.AppendItem(l1_item, f'Level 2 Item: {l2_row}')
                 self.tree.SetItemData(l2_item, None)
                 self.tree.SetItemImage(l2_item, self.il.file, wx.TreeItemIcon_Normal)
-                self.tree.SetItemImage(l2_item, self.il.fil_open, wx.TreeItemIcon_Selected)
+                self.tree.SetItemImage(l2_item, self.il.file_open, wx.TreeItemIcon_Selected)
 
         self.tree.ExpandAll()
+
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(self.tree, 0, wx.EXPAND)
+        self.SetSizer(sizer)
+
+        self.tree.Bind(wx.EVT_LEFT_DCLICK, self.OnLeftDClick)
+        self.tree.Bind(wx.EVT_RIGHT_DOWN, self.OnRightDown)
+        self.tree.Bind(wx.EVT_RIGHT_UP, self.OnRightUp)
+
+    def OnSize(self, event):
+        w, h = self.GetClientSize()
+        self.tree.SetSize(0, 0, w, h)
+
+    def OnLeftDClick(self, event):
+        pos = event.GetPosition()
+        item, flags = self.tree.HitTest(pos)
+        if item:
+            parent = self.tree.GetItemParent(item)
+            if parent.IsOk():
+                self.tree.SortChildren(item)
+        event.Skip()
+
+    def OnRightDown(self, event):
+        pos = event.GetPosition()
+        item, flag = self.tree.HitTest(pos)
+        if item:
+            self.tree.SelectItem(item)
+
+    def OnRightUp(self, event):
+        pos = event.GetPosition()
+        item, flags = self.tree.HitTest(pos)
+        if item:
+            self.tree.EditLabel(item)
+
+
+class TreeListPanelA(wx.Panel):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.Bind(wx.EVT_SIZE, self.OnSize)
+
+        self.tree = TreeListCtrlA(parent=self, id=wx.ID_ANY, pos=wx.DefaultPosition, size=wx.DefaultSize, style=wx.TR_DEFAULT_STYLE | wx.TR_FULL_ROW_HIGHLIGHT)
+
+        self.il = ImageList()
+        self.tree.SetImageList(self.il)
+
+        self.tree.AppendColumn("Main column")
+        self.tree.AppendColumn("Col 1")
+        self.tree.AppendColumn("Col 2")
+        self.tree.AppendColumn("Col 3")
+        # self.tree.SetColumnWidth(0, 200)
+
+        self.root = self.tree.GetRootItem()
+        self.child_1_a = self.tree.AppendItem(self.root, "Child 1 a")
+        self.child_1_b = self.tree.AppendItem(self.root, "Child 1 b")
+        self.child_2_a = self.tree.AppendItem(self.child_1_a, "Child 2 a")
+
+        self.tree.SetItemText(self.child_1_a, 1, "New text")
+
+        self.tree.SetItemImage(self.child_1_a, self.il.folder, self.il.file_open)
+
+        self.tree.Expand(self.root)
 
         sizer = wx.BoxSizer(wx.VERTICAL)
         sizer.Add(self.tree, 0, wx.EXPAND)
@@ -60,11 +146,27 @@ class TreePanel(wx.Panel):
         self.tree.SetSize(0, 0, w, h)
 
 
+class TreeListPanelB(wx.Panel):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.Bind(wx.EVT_SIZE, self.OnSize)
+
+        self.tree = TreeListCtrlB(parent=self, id=wx.ID_ANY, pos=wx.DefaultPosition, size=wx.DefaultSize, style=gizmos.TR_DEFAULT_STYLE | gizmos.TR_FULL_ROW_HIGHLIGHT)
+
+        # stuff goes here
+
+    def OnSize(self, event):
+        w, h = self.GetClientSize()
+        self.tree.SetSize(0, 0, w, h)
+
+
 class Frame(wx.Frame):
 
     def __init__(self):
         wx.Frame.__init__(self, parent=None, title='cG')
-        panel = TreePanel(self)
+        # panel = TreePanel(self)
+        # panel = TreeListPanelA(self)
+        panel = TreeListCtrlB(self)
         self.Show()
 
 
